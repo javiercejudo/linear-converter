01ccb7c13f6466ed136bd24a70b673ae5fb8d90b
Javier Cejudo <javier@javiercejudo.com>
2015-03-08 01:23:22 +1100

    feat(validation): add validation and proper tests

--- src/convert.js
+++ src/convert.js
@@ -2,19 +2,41 @@
 
 'use strict';
 
+var util = require('util');
 var rescale = require('rescale');
+var rescaleUtil = require('rescale-util');
 
 exports.PRESETS = require('./presets.json');
 
 exports.convert = function convert(x, preset) {
+  if (typeof preset === 'undefined') {
+    return x;
+  }
+
+  if (!rescaleUtil.isValidPreset(preset)) {
+    throw new Error(rescaleUtil.getLastError());
+  }
+
   return rescale.rescale(x, preset[0], preset[1]);
 };
 
 exports.invertPreset = function invertPreset(preset) {
+  if (!rescaleUtil.isValidPreset(preset)) {
+    throw new Error(rescaleUtil.getLastError());
+  }
+
   return preset.slice(0).reverse();
 };
 
 exports.composePresets = function composePresets(presets) {
+  if (!util.isArray(presets)) {
+    throw new Error('expected an Array of presets');
+  }
+
+  presets.forEach(function (preset) {
+    return rescaleUtil.isValidPreset(preset);
+  });
+
   return presets.reduce(function (previousPreset, currentPreset) {
     return [
       previousPreset[0],
--- test/spec.js
+++ test/spec.js
@@ -4,24 +4,176 @@
 
 var should = require('should');
 var sinon = require('sinon');
-var rescaler = require('rescale');
+var rescale = require('rescale');
+var rescaleUtil = require('rescale-util');
 var converter = require('../src/convert.js');
 
-describe('converter', function() {
-  var celsiusToFahrenheit = converter.PRESETS.temperature.celsiusToFahrenheit;
-  var fahrenheitToCelsius = converter.invertPreset(celsiusToFahrenheit);
-
-  var celsiusToKelvin = converter.PRESETS.temperature.celsiusToKelvin;
-  var kelvinToCelcius = converter.invertPreset(celsiusToKelvin);
-  var kelvinToFahrenheit = converter.composePresets([kelvinToCelcius, celsiusToFahrenheit]);
-
-  it.skip('should delegate to rescale', function () {
+describe('converting', function() {
+  describe('without a preset', function() {
+    it('should be the identity', function() {
+      converter.convert(1).should.be.exactly(1);
+      converter.convert(Math.E).should.be.exactly(Math.E);
+    });
   });
 
-  it('should convert correctly', function () {
-    converter.convert(25, celsiusToFahrenheit).should.be.exactly(77);
-    converter.convert(104, fahrenheitToCelsius).should.be.exactly(40);
+  describe('with a preset', function() {
+    var rescaleUtilMock;
 
-    converter.convert(293.15, kelvinToFahrenheit).should.be.exactly(68);
+    afterEach(function() {
+      rescaleUtilMock.verify();
+    });
+
+    it('should delegate its validation to rescale-util', function() {
+      rescaleUtilMock = sinon.mock(rescaleUtil);
+
+      rescaleUtilMock.expects('isValidPreset')
+        .withExactArgs([[0, 5], [0, 10]]).returns(true);
+
+      rescaleUtilMock.expects('isValidPreset')
+        .withExactArgs([[-5, 1], [Math.E, 10.2]]).returns(true);
+
+      converter.convert(2.5, [[0, 5], [0, 10]]);
+      converter.convert(-3, [[-5, 1], [Math.E, 10.2]]);
+    });
+  });
+
+  describe('with a valid preset', function() {
+    var rescaleStub;
+
+    beforeEach(function() {
+      rescaleStub = sinon.stub(rescale, 'rescale');
+
+      rescaleStub.withArgs('anything', [0, 10], [10, 20])
+        .onFirstCall().returns(Math.PI)
+        .onSecondCall().returns(34);
+    });
+
+    afterEach(function() {
+      rescaleStub.restore();
+    });
+
+    it('should delegate the conversion to rescale', function() {
+      converter.convert('anything', [[0, 10], [10, 20]]).should.be.exactly(Math.PI);
+      converter.convert('anything', [[0, 10], [10, 20]]).should.be.exactly(34);
+    });
+  });
+
+  describe('with an invalid preset', function() {
+    var isValidPresetStub, getLastErrorStub;
+
+    beforeEach(function() {
+      isValidPresetStub = sinon.stub(rescaleUtil, 'isValidPreset');
+      getLastErrorStub = sinon.stub(rescaleUtil, 'getLastError');
+
+      isValidPresetStub.returns(false);
+      getLastErrorStub.returns('an error');
+    });
+
+    afterEach(function() {
+      isValidPresetStub.restore();
+      getLastErrorStub.restore();
+    });
+
+    it('should throw an error', function() {
+      (function() {
+        converter.convert(2, 2);
+      }).should.throw('an error');
+    });
+  });
+});
+
+describe('inverting', function() {
+  describe('any preset', function() {
+    var rescaleUtilMock;
+
+    afterEach(function() {
+      rescaleUtilMock.verify();
+    });
+
+    it('should delegate its validation to rescale-util', function() {
+      rescaleUtilMock = sinon.mock(rescaleUtil);
+
+      rescaleUtilMock.expects('isValidPreset')
+        .withExactArgs([[0, 5], [0, 10]]).returns(true);
+
+      rescaleUtilMock.expects('isValidPreset')
+        .withExactArgs([[-5, 1], [Math.E, 10.2]]).returns(true);
+
+      converter.invertPreset([[0, 5], [0, 10]]);
+      converter.invertPreset([[-5, 1], [Math.E, 10.2]]);
+    });
+  });
+
+  describe('with an invalid preset', function() {
+    var isValidPresetStub, getLastErrorStub;
+
+    beforeEach(function() {
+      isValidPresetStub = sinon.stub(rescaleUtil, 'isValidPreset');
+      getLastErrorStub = sinon.stub(rescaleUtil, 'getLastError');
+
+      isValidPresetStub.returns(false);
+      getLastErrorStub.returns('some error');
+    });
+
+    afterEach(function() {
+      isValidPresetStub.restore();
+      getLastErrorStub.restore();
+    });
+
+    it('should throw an error', function() {
+      (function() {
+        converter.invertPreset(2, 2);
+      }).should.throw('some error');
+    });
+  });
+
+  describe('with a valid preset', function() {
+    it('should invert the preset', function() {
+      converter.invertPreset([[0, 10], [10, 20]]).should.eql([[10, 20], [0, 10]]);
+      converter.invertPreset([[-5, 4], [0.05, -5.4]]).should.eql([[0.05, -5.4], [-5, 4]]);
+    });
+  });
+});
+
+describe('composing', function() {
+  describe('with invalid input', function() {
+    it('should throw an error', function() {
+      (function() {
+        converter.composePresets(2, 2);
+      }).should.throw('expected an Array of presets');
+    });
+  });
+
+  describe('any presets', function() {
+    var rescaleUtilMock;
+
+    afterEach(function() {
+      rescaleUtilMock.verify();
+    });
+
+    it('should delegate their validation to rescale-util', function() {
+      rescaleUtilMock = sinon.mock(rescaleUtil);
+
+      rescaleUtilMock.expects('isValidPreset')
+        .withExactArgs([[0, 5], [0, 10]]).returns(true);
+
+      rescaleUtilMock.expects('isValidPreset')
+        .withExactArgs([[-5, 1], [Math.E, 10.2]]).returns(true);
+
+      converter.composePresets([[[0, 5], [0, 10]], [[-5, 1], [Math.E, 10.2]]]);
+    });
+  });
+
+  describe('with valid input', function() {
+    it('should compose the presets', function() {
+      converter.composePresets([[[0, 10], [10, 20]], [[10, 20], [50, 60]]])
+        .should.eql([[0, 10], [50, 60]]);
+
+      converter.composePresets([[[0, 1], [0, -2]], [[0, 3], [0, -9]]])
+        .should.eql([[0, 1], [0, 6]]);
+
+      converter.composePresets([[[1, 2], [2, 4]], [[1, 3], [3, 9]], [[1000, Math.E], [999, Math.E - 1]]])
+        .should.eql([[1, 2], [5, 11]]);
+    });
   });
 });
