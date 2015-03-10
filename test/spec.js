0d20eab9345ecfd83bdfbce0f8a803adf232c142
Javier Cejudo <javier@javiercejudo.com>
2015-03-11 00:56:12 +1100

    refactor(spec): use betterer package

--- test/spec.js
+++ test/spec.js
@@ -2,33 +2,29 @@
 
 'use strict';
 
+var better = require('betterer').better;
 var convertTests = require('./convert/convertTests.js');
 var invertPresetTests = require('./invertPreset/invertPresetTests.js');
 var composePresetsTests = require('./composePresets/composePresetsTests.js');
 
 var o_o = describe;
-var camelCase = require('camel-case');
 
-function better(description, context) {
-  return context[camelCase(description)];
-}
-
-o_o('converting', function() {
-  o_o('without a preset', better('be the identity', convertTests));
-  o_o('with a preset', better('delegate its validation to rescale util', convertTests));
-  o_o('with a valid preset', better('delegate the conversion to rescale', convertTests));
-  o_o('with an invalid preset', better('throw an error', convertTests));
+o_o('converting', function() { var o_O = convertTests;
+  o_o('without a preset', better('be the identity', o_O));
+  o_o('with a preset', better('delegate its validation to rescale util', o_O));
+  o_o('with a valid preset', better('delegate the conversion to rescale', o_O));
+  o_o('with an invalid preset', better('throw an error', o_O));
 });
 
-o_o('inverting', function() {
-  o_o('any preset', better('delegate its validation to rescale util', invertPresetTests));
-  o_o('with an invalid preset', better('throw an error', invertPresetTests));
-  o_o('with a valid preset', better('invert the preset', invertPresetTests));
+o_o('inverting', function() { var o_O = invertPresetTests;
+  o_o('any preset', better('delegate its validation to rescale util', o_O));
+  o_o('with an invalid preset', better('throw an error', o_O));
+  o_o('with a valid preset', better('invert the preset', o_O));
 });
 
-o_o('composing', function() {
-  o_o('with invalid input', better('throw an error', composePresetsTests));
-  o_o('any presets', better('delegate their validation to rescale util', composePresetsTests));
-  o_o('with invalid presets', better('throw another error', composePresetsTests));
-  o_o('with valid presets', better('compose the presets', composePresetsTests));
+o_o('composing', function() { var o_O = composePresetsTests;
+  o_o('with invalid input', better('throw an error', o_O));
+  o_o('any presets', better('delegate their validation to rescale util', o_O));
+  o_o('with invalid presets', better('throw another error', o_O));
+  o_o('with valid presets', better('compose the presets', o_O));
 });
