56f79edc2aa5dc52de527347682dbee949e80297
Javier Cejudo <javier@javiercejudo.com>
2015-03-20 00:34:21 +1100

    feat(presets): add area presets

--- test/presets.js
+++ test/presets.js
@@ -184,8 +184,24 @@ describe('built-in presets', function() {
     should(converter.PRESETS.volume).be.an.Object.and.not.eql({});
   });
 
-  it.skip('should include area', function() {
-    should(converter.PRESETS.area).be.an.Object.and.not.eql({});
+  it('should include area', function() {
+    var area = presets.area;
+
+    (5.045e+9).should.be.exactly(convert(5045, invert(area.squareMetreToSquareKilometre)), 'squareMetreToSquareKilometre')
+      .and.exactly(convert(504500, invert(area.squareMetreToHectare)), 'squareMetreToHectare')
+      .and.exactly(convert(1947.8853898466393, invert(area.squareMetreToSquareMile)), 'squareMetreTosquareMile')
+      .and.exactly(convert(1246646.6495018492, invert(area.squareMetreToAcre)), 'squareMetreToAcre')
+      .and.exactly(convert(6033769783.58895, invert(area.squareMetreToSquareYard)), 'squareMetreToSquareYard')
+      .and.exactly(convert(54303928052.300545, invert(area.squareMetreToSquareFoot)), 'squareMetreToSquareFoot')
+      .and.exactly(convert(7819765639531.279, invert(area.squareMetreToSquareInch)), 'squareMetreToSquareInch');
+
+    (0).should.be.exactly(convert(0, area.squareMetreToSquareKilometre), 'squareMetreToSquareKilometre')
+      .and.exactly(convert(0, area.squareMetreToHectare), 'squareMetreToHectare')
+      .and.exactly(convert(0, area.squareMetreTosquareMile), 'squareMetreTosquareMile')
+      .and.exactly(convert(0, area.squareMetreToAcre), 'squareMetreToAcre')
+      .and.exactly(convert(0, area.squareMetreToSquareYard), 'squareMetreToSquareYard')
+      .and.exactly(convert(0, area.squareMetreToSquareFoot), 'squareMetreToSquareFoot')
+      .and.exactly(convert(0, area.squareMetreToSquareInch), 'squareMetreToSquareInch');
   });
 
   it.skip('should include fuel consumption', function() {
