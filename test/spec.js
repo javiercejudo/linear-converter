0e7ba0655268ddda3aad2895191a88b49f268860
Javier Cejudo <javier@javiercejudo.com>
2015-03-23 01:09:47 +1100

    chore(spec): modify wording of spec descriptions

--- test/spec.js
+++ test/spec.js
@@ -20,23 +20,23 @@ o_o('converting', function() { var o_O = convertTests;
 
 o_o('inverting', function() { var o_O = invertPresetTests;
   o_o('any preset', better('delegate its validation to rescale util', o_O));
-  o_o('with an invalid preset', better('throw an error', o_O));
-  o_o('with a valid preset', better('invert the preset', o_O));
+  o_o('an invalid preset', better('throw an error', o_O));
+  o_o('a valid preset', better('invert the preset', o_O));
 });
 
 o_o('composing', function() { var o_O = composePresetsTests;
   o_o('with invalid input', better('throw an error', o_O));
   o_o('any presets', better('delegate their validation to rescale util', o_O));
-  o_o('with invalid presets', better('throw another error', o_O));
-  o_o('with valid presets', better('compose the presets', o_O));
+  o_o('invalid presets', better('throw another error', o_O));
+  o_o('valid presets', better('compose the presets', o_O));
 });
 
 o_o('computing coefficient a', function() { var o_O = coefficientATests;
-  o_o('with a valid preset', better('return coefficient a', o_O));
-  o_o('with an invalid preset', better('throw an error', o_O));
+  o_o('of a valid preset', better('return coefficient a', o_O));
+  o_o('of an invalid preset', better('throw an error', o_O));
 });
 
 o_o('computing coefficient b', function() { var o_O = coefficientBTests;
-  o_o('with a valid preset', better('return coefficient b', o_O));
-  o_o('with an invalid preset', better('throw an error', o_O));
+  o_o('of a valid preset', better('return coefficient b', o_O));
+  o_o('of an invalid preset', better('throw an error', o_O));
 });
