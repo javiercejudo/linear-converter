943473721b3bb99453b75759385bacdce8f95df0
Javier Cejudo <javier@javiercejudo.com>
2015-05-13 00:06:27 +1000

    refactor(test): split browser tests

--- test/browser/browser.js
+++ test/browser/browser.js
@@ -1,5 +1,5 @@
-/*jshint node:true, mocha:true */
-/*global converter */
+/*jshint mocha:true */
+/*global require, Should */
 
 (function($hould) {
   'use strict';
@@ -7,30 +7,36 @@
   var converter = require('linear-converter');
 
   describe('browser support', function() {
-    it('should cover the basics', function() {
-      // main converter method
-      var convert = converter.convert;
+    // main converter method
+    var convert = converter.convert;
 
-      // built-in presets for temperature, length, mass and more
-      var temp = converter.PRESETS.temperature;
+    // built-in presets for temperature, length, mass and more
+    var temp = converter.PRESETS.temperature;
 
+    // easy inversion of presets
+    var invert = converter.invertPreset;
+
+    // convert presets any to any using inversion and composition
+    var compose = converter.composePresets;
+
+    it('should support convertion', function() {
       convert(25, temp.celsiusToFahrenheit).should.be.exactly(77);
+    });
 
-      // easy inversion of presets
-      var invert = converter.invertPreset;
-
+    it('should support preset invertion', function() {
       convert(77, invert(temp.celsiusToFahrenheit)).should.be.exactly(25);
+    });
 
-      // convert presets any to any using inversion and composition
-      var compose = converter.composePresets;
-
+    it('should support preset composition', function() {
       var kelvinToFahrenheit = compose([
         invert(temp.celsiusToKelvin),
         temp.celsiusToFahrenheit
       ]);
 
       convert(293.15, kelvinToFahrenheit).should.be.exactly(68);
+    });
 
+    it('should support calculating coefficients', function() {
       // calculate the coefficients for the underlying
       // linear function from a preset
       // using Should() to make IE9 happy: https://github.com/tj/should.js/wiki/Known-Bugs#ie9
