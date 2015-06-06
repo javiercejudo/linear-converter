816093c1d1c07cb1d51bd5c4f8f5bc9856edd87c
Javier Cejudo <javier@javiercejudo.com>
2015-06-06 16:05:58 +1000

    1.2.0

--- dist/linear-converter.js
+++ dist/linear-converter.js
@@ -1,6 +1,6 @@
 /**
  * linear-converter - Copyright 2015 Javier Cejudo <javier@javiercejudo.com> (http://www.javiercejudo.com)
- * @version v1.2.0-beta.1
+ * @version v1.2.0
  * @link https://github.com/javiercejudo/linear-converter#readme
  * @license MIT
  */
