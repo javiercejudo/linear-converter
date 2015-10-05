fab0e7d15e7d139ad8326e4ed504d0d3b5e9e48d
Javier Cejudo <javier@javiercejudo.com>
2015-10-05 16:39:39 +1100

    7.0.1

--- dist/linear-converter.min.js
+++ dist/linear-converter.min.js
@@ -1,6 +1,6 @@
 /**
  * linear-converter - Copyright 2015 Javier Cejudo <javier@javiercejudo.com> (http://www.javiercejudo.com)
- * @version v7.0.0
+ * @version v7.0.1
  * @link https://github.com/javiercejudo/linear-converter#readme
  * @license MIT
  */
